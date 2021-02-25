e6e97675738fa8eeacfc38896dc2aab4426e8f7c
Afonso -aptsharp <aff-on@hotmail.com>
2021-02-25 14:38:34 -0300

    Aula 02

---
+++ Classes/Serie.cs
@@ -0,0 +1,46 @@
+using System;
+namespace Series.Classes
+{
+    public class Serie : EntidadeBase
+    {
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+
+        //Construtor    
+        public Serie(Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = Id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero + Environment.NewLine;
+            retorno += "Titulo: " + this.Titulo + Environment.NewLine;
+            retorno += "Descricao: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano: " + this.Ano + Environment.NewLine;
+            return retorno;
+            //Envrironment depende do sistema operacional.
+        }
+
+        //encapsulamento
+
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+        public int retrnaId()
+        {
+            return this.Id;
+        }
+
+    }
+
+}
